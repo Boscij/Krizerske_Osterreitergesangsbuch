cee90134e2af0a37938c6de3b36d3fe0cf9584b9
Boscij <BoscijD@gmx.de>
2022-03-12 21:46:34 +0100

    First release

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -7,7 +7,6 @@
 
 import 'package:file_picker/src/file_picker_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
-import 'package:firebase_storage_web/firebase_storage_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -15,6 +14,5 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins(Registrar registrar) {
   FilePickerWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
-  FirebaseStorageWeb.registerWith(registrar);
   registrar.registerMessageHandler();
 }
