be439711fb57577cbb056bec90174f0a6211fcce
Boscij <BoscijD@gmx.de>
2022-03-12 12:54:13 +0100

    add feature to view pdf from assets and add two buttons on mainscreen

---
+++ lib/button/button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ButtonWidget extends StatelessWidget {
+  final String text;
+  final VoidCallback onClicked;
+
+  const ButtonWidget({
+    Key key,
+    @required this.text,
+    @required this.onClicked,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Color.fromARGB(255, 130, 225, 173),
+          minimumSize: Size.fromHeight(50),
+        ),
+        child: Text(text, style: TextStyle(fontSize: 20, color: Colors.white)),
+        onPressed: onClicked,
+      );
+}
