4c4048aaf406e11ee948a2426f891a65b521b9b1
Boscij <BoscijD@gmx.de>
2023-03-08 07:16:27 +0100

    Next release

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -4,6 +4,7 @@
 
 // ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: depend_on_referenced_packages
 
 import 'package:file_picker/src/file_picker_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
@@ -11,7 +12,7 @@ import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
-void registerPlugins(Registrar registrar) {
+void registerPlugins(final Registrar registrar) {
   FilePickerWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   registrar.registerMessageHandler();
